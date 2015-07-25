896677ff2d2cbeb84d9aab2f281e2e99a517af24
Kenny Grant <kennygrant@gmail.com>
2015-07-25 21:31:58 +0100

    Updated auth functions, reorganised files, added random token

--- auth.go
+++ auth.go
@@ -1,88 +1,54 @@
-// Basic authentication and authorisation for web tasks
+// Package auth provides helpers for encryption and passwords.
 package auth
 
 import (
-    "encoding/hex"
-    "encoding/base64"
+	"encoding/base64"
 
-    // A vendored version of the golang bcrypt pkg
+	// A vendored version of the golang bcrypt pkg
 	"github.com/fragmenta/auth/internal/bcrypt"
 )
 
-// BCRYPT HASHES
-
-
-// This should remain constant or hashed passwords will need to be recalculated
+// For bcrypt hashes - this should remain constant or hashed passwords will need to be recalculated
 var HashCost = 10
 
-
-// Compare a password hashed with bcrypt
-func CheckPassword(plain_password, encrypted_password string) error {
-	return bcrypt.CompareHashAndPassword([]byte(encrypted_password),[]byte(plain_password))
+// CheckPassword compares a password hashed with bcrypt
+func CheckPassword(pass, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(pass))
 }
 
-
-// Hash a password with a random salt using bcrypt.
-func EncryptPassword(plain_password string) (string, error) {
-    b,err := bcrypt.GenerateFromPassword([]byte(plain_password),HashCost)
-	return string(b),err
+// EncryptPassword hashes a password with a random salt using bcrypt.
+func EncryptPassword(pass string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass), HashCost)
+	return string(hash), err
 }
-
-
-// DEPRECATED - REMOVE BUT EXPERIMENT WITH EFFECT ON BOOKINGS FIRST
-
-// Convert a hex string representation of bytes to a byte representation 
-// (which may be unprintable)
-func HexToBytes(h string) []byte {
-    s,err := hex.DecodeString(h)
-    if err != nil {
-        s = []byte("ERROR")
-    }
-    return s
-}
-
-// Convert bytes to a hex string representation of bytes
-func BytesToHex(b []byte) string {
-    return hex.EncodeToString(b)
-}
-
-
-
-// ENCRYPTION
-
-
-
 
 // TODO For CSRF below, we should include a time token at the end of the string
 // and validate it is correct down to a given window (say 1 hour) - after that the token expires
 
 // TODO http://www.thoughtcrime.org/blog/the-cryptographic-doom-principle/
-// Encrypt Then Authenticate 
+// Encrypt Then Authenticate
 // The sender encrypts the plaintext, then appends a MAC of the ciphertext. Ek1(P) || MACk2(Ek1(P))
 
 // TODO actually encrypt, don't just hash the CSRF
 
-// Compare  plain text with a string encrypted by bcrypt as a csrf token
-func CheckCSRFToken(plain_text, b64 string) error {
-    // First base64 decode the value
-    encrypted := make([]byte,256)
-    _, err := base64.URLEncoding.Decode(encrypted,[]byte(b64))
-    if err != nil {
-        return err
-    }
-    
-	return bcrypt.CompareHashAndPassword(encrypted,[]byte(plain_text))
+// CheckCSRFToken compares a plain text with a string encrypted by bcrypt as a csrf token
+func CheckCSRFToken(token, b64 string) error {
+	// First base64 decode the value
+	encrypted := make([]byte, 256)
+	_, err := base64.URLEncoding.Decode(encrypted, []byte(b64))
+	if err != nil {
+		return err
+	}
+
+	return bcrypt.CompareHashAndPassword(encrypted, []byte(token))
 }
 
+// CSRFToken encrypts a string with a random salt using bcrypt.
+func CSRFToken(token string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(token), HashCost)
+	if err != nil {
+		return "", err
+	}
 
-// Encrypt a string with a random salt using bcrypt.
-func CSRFToken(plain_text string) (string, error) {
-    b,err := bcrypt.GenerateFromPassword([]byte(plain_text),HashCost)
-    if err != nil {
-        return "", err
-    }
-    
 	return base64.URLEncoding.EncodeToString(b), nil
 }
-
-
--- session.go
+++ session.go
@@ -1,148 +1,145 @@
+// Package auth provides helpers for encryption and passwords.
 package auth
 
-
 import (
-    "errors"
-    "net/http"
-    "fmt"
-    "time"
-    
-    // We should vendor this dependency... or simply remove as it is likely very simple and we only need encryption
-    "github.com/fragmenta/auth/internal/cookie"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+	// TODO - remove dependency as we don't use much of it
+	"github.com/fragmenta/auth/internal/cookie"
 )
 
-// These should be set on app startup from ENV variables
-// Should these be in the session store object, not out here as globals?
-var HMACKey          []byte
-var SecretKey        []byte
-var SessionName      string
-var SecureCookies    bool
-var SessionUserKey   string
+// These should be set on app startup
 
+// The key for generating HMAC
+var HMACKey []byte
 
-// Interface for a session store (backed by unknown storage)
+// The key for encrypting content
+var SecretKey []byte
+
+// The session name
+var SessionName string
+
+// Should we use secure cookies?
+var SecureCookies bool
+
+// The session user key
+var SessionUserKey string
+
+// SessionStore is the interface for a session store (backed by unknown storage)
 type SessionStore interface {
-    Get(string) string
-    Set(string,string) 
-    Load(request *http.Request) error
-    Save(http.ResponseWriter) error
-    Clear(http.ResponseWriter)
+	Get(string) string
+	Set(string, string)
+	Load(request *http.Request) error
+	Save(http.ResponseWriter) error
+	Clear(http.ResponseWriter)
 }
 
-
-// Concrete version of SessionStore, which stores the information encrypted with bcrypt in cookies.
+// CookieSessionStore is a Concrete version of SessionStore, which stores the information encrypted with bcrypt in cookies.
 type CookieSessionStore struct {
-    values  map[string]string
+	values map[string]string
 }
 
-
+// init the package
 func init() {
-    // HttpOnly is on by default
-    SecureCookies       = false // off by default
-    SessionName         = "fragmenta_session"
-    SessionUserKey      = "user_id"
+	// HttpOnly is on by default
+	SecureCookies = false // off by default
+	SessionName = "fragmenta_session"
+	SessionUserKey = "user_id"
 }
 
+// Session loads or create the current session
+func Session(writer http.ResponseWriter, request *http.Request) (SessionStore, error) {
 
-// Fetch or create the current session
-func Session(writer http.ResponseWriter,request *http.Request) (SessionStore, error) {
-    
-    
-    if len(HMACKey) == 0 || len(SecretKey) == 0 {
-        return nil,errors.New("Authentication secrets not initialised")
-    }
-    
-    // Return the current session store from cookie or a new one if none found
-    s := &CookieSessionStore{
-        values:make(map[string]string,0),
+	if len(HMACKey) == 0 || len(SecretKey) == 0 {
+		return nil, errors.New("Authentication secrets not initialised")
 	}
-    
-    // Check if the session exists
-    err := s.Load(request)
-    if err != nil {
-       // If no session, write it out for the first time (empty)
-       fmt.Printf("Error on cookie load: %s\n",err)
-       s.Save(writer)
-       return s,nil
-    }
-    
-    return s,nil
-}
 
+	// Return the current session store from cookie or a new one if none found
+	s := &CookieSessionStore{
+		values: make(map[string]string, 0),
+	}
+
+	// Check if the session exists
+	err := s.Load(request)
+	if err != nil {
+		// If no session, write it out for the first time (empty)
+		fmt.Printf("Error on cookie load: %s\n", err)
+		s.Save(writer)
+		return s, nil
+	}
+
+	return s, nil
+}
 
 // Get a value from the session
 func (s *CookieSessionStore) Get(key string) string {
-   return s.values[key]
+	return s.values[key]
 }
 
 // Set a value in the session - NB you must call Save after this if you wish to save
-func (s *CookieSessionStore) Set(key string,value string)  {
-   s.values[key] = value 
+func (s *CookieSessionStore) Set(key string, value string) {
+	s.values[key] = value
 }
 
+// Load the session from cookie
 func (s *CookieSessionStore) Load(request *http.Request) error {
-    
-    // Just replace this with your own methods on CookieSessionStore...
-    cookieMonster := cookie.New(HMACKey,SecretKey)
-  
-    cookie, err := request.Cookie(SessionName)
-    if err != nil {
-        return err
-    }
-    
-  //  fmt.Printf("Cookie load: %v\n%v\n",err,cookie)
-    
-    
-    // Read the encrypted values back out into our values in the session
-    err = cookieMonster.Decode(SessionName, cookie.Value, &s.values);
-    if err != nil {
-        return err
-    }
-    
-    return nil
+
+	// Just replace this with your own methods on CookieSessionStore...
+	cookieMonster := cookie.New(HMACKey, SecretKey)
+
+	cookie, err := request.Cookie(SessionName)
+	if err != nil {
+		return err
+	}
+
+	//  fmt.Printf("Cookie load: %v\n%v\n",err,cookie)
+
+	// Read the encrypted values back out into our values in the session
+	err = cookieMonster.Decode(SessionName, cookie.Value, &s.values)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-
+// Save the session to a cookie
 func (s *CookieSessionStore) Save(writer http.ResponseWriter) error {
-  //  println("SAVING SESSION")
- 
-    
-    // Just replace this with your own methods on CookieSessionStore...
-    cookieMonster := cookie.New(HMACKey, SecretKey)
-  
+	//  println("SAVING SESSION")
 
-    encrypted, err := cookieMonster.Encode(SessionName, s.values)
-    if err != nil {
-        return err
-    }
-    
-    cookie  := &http.Cookie{
-    			Name:  SessionName,
-    			Value: encrypted,
-                HttpOnly: true,
-                Secure: SecureCookies,
-            	Path:  "/",
-                Expires: time.Now().AddDate(0,0,7),// Expires in seven days
-    		
-    		 }
-             
-              
-   http.SetCookie(writer,cookie)
-	
-   return nil
+	// Just replace this with your own methods on CookieSessionStore...
+	cookieMonster := cookie.New(HMACKey, SecretKey)
+
+	encrypted, err := cookieMonster.Encode(SessionName, s.values)
+	if err != nil {
+		return err
+	}
+
+	cookie := &http.Cookie{
+		Name:     SessionName,
+		Value:    encrypted,
+		HttpOnly: true,
+		Secure:   SecureCookies,
+		Path:     "/",
+		Expires:  time.Now().AddDate(0, 0, 7), // Expires in seven days
+
+	}
+
+	http.SetCookie(writer, cookie)
+
+	return nil
 }
 
-// THIS IS BROKEN - NOT CLEARING COOKIE PROPERLY - INVESTIGATE
+// Clear the session values from the cookie
+func (s *CookieSessionStore) Clear(writer http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   SessionName,
+		Value:  "",
+		MaxAge: -1,
+		Path:   "/",
+	}
 
-// Clear the session values and save out
-func (s *CookieSessionStore) Clear(writer http.ResponseWriter)  {
-    cookie  := &http.Cookie{
-    			Name:  SessionName,
-    			Value: "",
-                MaxAge: -1,
-    			Path:  "/",
-    		 }
-          
-   http.SetCookie(writer,cookie)
+	http.SetCookie(writer, cookie)
 }
-
